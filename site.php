291f9993e105b28791e116bf9e20a99365dfdf1e
edineysouza <edineysouza@edineysouza.lan>
2018-09-23 17:37:03 -0300

    Carrinho de compras

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
 
 $app->config('debug', true);
 
@@ -57,6 +58,17 @@ $app->get("/products/:desurl", function($desurl){
 		'product'=>$product->getValues(),
 		'categories'=>$product->getCategories()
 	]);
+});
+
+$app->get("/cart", function(){
+
+	$cart = Cart::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("cart");
+
+	
 });
 
 ?>
