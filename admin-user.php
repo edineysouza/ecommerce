6a0de54f51785f48bc8c98e97aa1aaddea12a202
edineysouza <edineysouza@edineysouza.lan>
2018-10-11 22:44:38 -0300

    Admin - Produtos Paginação e Busca

--- admin-user.php
+++ admin-user.php
@@ -26,13 +26,13 @@ $app->get("/admin/users", function(){
 
 		array_push($pages, [
 			'href'=>'/admin/users?'.http_build_query([
-				'page'=>$x+1,
+				'page'=>$x + 1,
 				'search'=>$search
 			]),
-			'text'=>$x+1
+			'text'=>$x + 1
 		]);
 	}
-
+	
 	$page = new PageAdmin();
 
 	$page->setTpl("users",  array(
